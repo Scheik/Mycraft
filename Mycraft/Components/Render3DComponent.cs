487f74b1156495897438b5c21603537e8ca1c432
Scheik <scheik.todeswache@googlemail.com>
2015-12-27 22:45:47 +0100

    update

--- Mycraft/Components/Render3DComponent.cs
+++ Mycraft/Components/Render3DComponent.cs
@@ -10,9 +10,11 @@ namespace Mycraft.Components
 {
     internal sealed class Render3DComponent : DrawableGameComponent
     {
-        VertexPositionNormalTexture[] vertices;
         BasicEffect effect;
         Texture2D dirt;
+
+        VertexBuffer vb;
+        IndexBuffer ib;
 
         public Render3DComponent(Game game) 
             : base(game)
@@ -23,24 +25,40 @@ namespace Mycraft.Components
         protected override void LoadContent()
         {
 
-            vertices = new VertexPositionNormalTexture[] {
+            VertexPositionNormalTexture[] vertices = new VertexPositionNormalTexture[] {
                 new VertexPositionNormalTexture(new Vector3(-1, 1, 0), Vector3.Forward, new Vector2(0,0)),
                 new VertexPositionNormalTexture(new Vector3(1,1,0), Vector3.Forward, new Vector2(1,0)),
-                new VertexPositionNormalTexture(new Vector3(1,-1,0), Vector3.Forward, new Vector2(1,1)),
-                new VertexPositionNormalTexture(new Vector3(-1, 1, 0), Vector3.Forward, new Vector2(0,0)),
                 new VertexPositionNormalTexture(new Vector3(1,-1,0), Vector3.Forward, new Vector2(1,1)),
                 new VertexPositionNormalTexture(new Vector3(-1,-1,0), Vector3.Forward, new Vector2(0,1)),
             };
 
+            short[] index = new short[]
+            {
+                0,1,2,0,2,3
+            };
+
+            vb = new VertexBuffer(GraphicsDevice, VertexPositionNormalTexture.VertexDeclaration, 6, BufferUsage.WriteOnly);
+            vb.SetData<VertexPositionNormalTexture>(vertices);
+
+            ib = new IndexBuffer(GraphicsDevice, IndexElementSize.SixteenBits, 6, BufferUsage.WriteOnly);
+            ib.SetData<short>(index);
+
             dirt = Game.Content.Load<Texture2D>("Textures/dirt");
 
             effect = new BasicEffect(GraphicsDevice);
-           // effect.EnableDefaultLighting();
             effect.World = Matrix.Identity;
             effect.View = Matrix.CreateLookAt(new Vector3(0,0,10), Vector3.Zero, Vector3.Up);
             effect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1f, 10000f);
             effect.TextureEnabled = true;
             effect.Texture = dirt;
+
+            //effect.EnableDefaultLighting();
+            //effect.LightingEnabled = true;
+            //effect.AmbientLightColor = Color.DarkGray.ToVector3();
+
+            //effect.DirectionalLight0.Enabled = true;
+            //effect.DirectionalLight0.Direction = new Vector3(-3, -3, -5);
+            //effect.DirectionalLight0.DiffuseColor = Color.Red.ToVector3();
 
             base.LoadContent();
         }
@@ -57,13 +75,18 @@ namespace Mycraft.Components
         public override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.RasterizerState = RasterizerState.CullNone;
 
             effect.World = Matrix.CreateRotationY(rotY);
+            GraphicsDevice.SetVertexBuffer(vb);
+            GraphicsDevice.Indices = ib;
 
             foreach (var pass in effect.CurrentTechnique.Passes)
             {
                 pass.Apply();
-                GraphicsDevice.DrawUserPrimitives<VertexPositionNormalTexture>(PrimitiveType.TriangleList, vertices, 0, 2);
+                //GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 2);
+                GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 4, 0, 2);
+                //GraphicsDevice.DrawUserPrimitives<VertexPositionNormalTexture>(PrimitiveType.TriangleList, vertices, 0, 2);
             }
         }
     }
